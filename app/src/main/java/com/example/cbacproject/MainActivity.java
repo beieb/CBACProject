d57c0538b82fa3cc9891e96f46d9c11bae3b300c
v <116445593+beieb@users.noreply.github.com>
2024-03-05 16:12:59 +0100

    toolbar
    la toolbar refaite au propre

--- app/src/main/java/com/example/cbacproject/MainActivity.java
+++ app/src/main/java/com/example/cbacproject/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.cbacproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page_no_connect);
+        Toolbar myToolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setTitle("HOME");
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        TextView txt;
+        if (item.getItemId() == R.id.home) {
+            Log.d("CBAC", "home yes");
+            return true;
+        } else if (item.getItemId() == R.id.user){
+            Log.d("CBAC", "user yes");
+            return true;
+        }else if (item.getItemId() == R.id.map) {
+            Log.d("CBAC", "map yes");
+            return true;
+        } else if (item.getItemId() == R.id.mountaineer){
+            Log.d("CBAC", "mountaineer yes");
+            return true;
+        }
+        return false;
+    }
+
 }
